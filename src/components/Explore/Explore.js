83dac3f4761bbb36a04310790d4cc82c43443279
Hayden <anhazzing33@gmail.com>
2018-06-05 15:18:51 -0500

    commit

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -3,8 +3,6 @@ import "./Explore.css";
 import MyContext from "../../MyContext";
 import Hexa_1 from "./Hexa_1/Hexa_1";
 import Hexa_2 from "./Hexa_2/Hexa_2";
-import Hexa_3 from "./Hexa_3/Hexa_3";
-
 import Space from "./Space/Space";
 class Explore extends Component {
   render() {
@@ -14,7 +12,6 @@ class Explore extends Component {
           <div className={context.state.explore ? "Explore" : null}>
             <Hexa_1 />
             <Hexa_2 />
-            {/* <Hexa_3 /> */}
             <Space />
           </div>
         )}
